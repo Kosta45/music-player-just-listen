5bf37edafaa58f53ce93e9bef03cb961b0bb0196
Kosta45 <konstanlebed8@gmail.com>
2023-08-04 18:17:12 +0300

    Work top list. Work player. Need update swiper.

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -5,15 +5,11 @@ import { genres } from "../assets/constants";
 
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
-// state = {};
-
 const Discover = () => {
   const dispatch = useDispatch();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = "Pop";
-
-  console.log(data);
 
   if (isFetching) return <Loader title="Loading songs..." />;
 
@@ -22,7 +18,7 @@ const Discover = () => {
   return (
     <div className="flex flex-col">
       <div
-        className="w-full flex justify-betwenn items-center
+        className="w-full flex justify-between items-center
       sm:flex-row flex-col mt-4 mb-10"
       >
         <h2 className="font-bold text-3xl text-white text-left">
